aaa2b953e1f5cc7388fb5598fa84d3722f576f9d
Subnet <rt_fan@mail.ru>
2023-01-05 22:15:09 +0300

    update

--- python_book/test_class.py
+++ python_book/test_class.py
@@ -1,5 +1,18 @@
 from random import randint
 
 
-for i in range(10):
-    print(randint(1, 6))
+class Die:
+    def __init__(self):
+        self.sides = 6
+
+    def roll_die(self):
+        print(randint(1, self.sides))
+
+
+q = Die()
+# for i in range(10):
+#     q.roll_die()
+
+w = Die()
+w.sides = 10
+w.roll_die()
